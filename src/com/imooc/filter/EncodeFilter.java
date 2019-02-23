9755193f4367eb325248dfebc8d341f5acd795cd
smallForest <1032817724@qq.com>
2019-02-23 11:10:34 +0800

    add init-param to wex.xml filter-config

--- src/com/imooc/filter/EncodeFilter.java
+++ src/com/imooc/filter/EncodeFilter.java
@@ -7,17 +7,19 @@ import java.io.IOException;
  * 字符过滤器
  */
 public class EncodeFilter implements Filter {
+    private String encoding;
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         //把请求的修改成为utf-8
-        servletRequest.setCharacterEncoding("UTF-8");
-        servletResponse.setCharacterEncoding("UTF-8");
+        servletRequest.setCharacterEncoding(this.encoding);
+        servletResponse.setCharacterEncoding(this.encoding);
         //告诉其他过滤器本过滤器已经完成过滤工作
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        this.encoding = filterConfig.getInitParameter("encode");
         System.out.println("字符过滤器启动》》》");
     }
 
